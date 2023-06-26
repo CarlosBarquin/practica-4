987405da67fb7f34a34e93fa83d6d81b6a1184bb
CarlosBarquin <116747942+CarlosBarquin@users.noreply.github.com>
2023-06-26 12:03:56 +0200

    Delete src directory

--- src/resolvers/query.ts
+++
@@ -1,8 +0,0 @@
-import { slotCollection } from "../db/dbconnection.ts";
-import { ObjectId } from "mongo";
-import { Slot } from "../types.ts";
-
-export const Query = {
-  test : () => "Hello World",
-
-};
--- src/types.ts
+++
@@ -1,8 +0,0 @@
-export type Slot = {
-    day: number;
-    month: number;
-    year: number;
-    hour: number;
-    available: boolean;
-    dni?: string;
-  };
